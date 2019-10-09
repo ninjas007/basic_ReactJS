28ece82fff3acce117224d93a00d342ec647e22a
Tilis Tiadi <tiliztiadi@gmail.com>
2019-10-09 21:52:26 +0700

    mixed all learn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Salam(props) {
+  return <h1> Halo {props.name} - Usia {props.age}</h1>
+}
+
+class Timer extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      time : props.start
+    }
+  }
+  
+  // Lifecycle
+  componentDidMount() {
+    this.addInterval = setInterval( () => {
+      this.increase()
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval)
+  }
+
+  increase() {
+    if (this.state.time == "20") {
+      alert('sudah sampai 20')
+    }
+    // parameter 1 = state sebelumnya
+    // parameter 2 = properti kalo dibutuhkan
+    this.setState( (state, props) => ({
+      time : parseInt(state.time) + 1
+    }))
+  }
+
+  render() {
+    return <div>Timer : {this.state.time}</div>;
+  }
+}
 
 class ToggleClick extends Component {
   constructor(props) {
@@ -35,6 +74,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          {/* <Salam name="Tilis Tiadi" age="25" /> */}
+          {/* <Timer start="0"/> */}
           <ToggleClick />
         </header>
       </div>
